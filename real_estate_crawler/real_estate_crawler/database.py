bf5bbca3ce22c9749de0ba8a69025766f9839c44
JosielJunior <josiel-junior2@hotmail.com>
2023-08-09 20:38:31 -0300

    chore: raise db pool

--- real_estate_crawler/real_estate_crawler/database.py
+++ real_estate_crawler/real_estate_crawler/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from .models import Base
 
 engine = create_engine("mysql://root:secret@db/real_estate_crawler",
-                       pool_size=20,  
+                       pool_size=40,  
                        max_overflow=10)
 
 Base.metadata.create_all(engine)
